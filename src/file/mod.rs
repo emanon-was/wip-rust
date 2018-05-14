7a25086e18cdb15a97dc9b92565ad2be85dff936
Daigo Kawasaki <kawasaki@fujisan.co.jp>
2018-05-14 18:05:11 +0900

    fileモジュールを整理

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -5,53 +5,57 @@ use std::fs::File;
 use std::io::Read;
 
 #[derive(Debug)]
-pub struct TextFile {
+pub struct ReadFile {
     path: String,
     body: String
 }
 
-impl TextFile {
-    pub fn new(path: &str, body: &str) -> TextFile {
-        TextFile {
-            path: path.to_string(),
-            body: body.to_string()
+impl ReadFile {
+    pub fn new(path: &str, body: &str) -> ReadFile {
+        ReadFile {
+            path: String::from(path),
+            body: String::from(body)
         }
     }
 }
 
 // derive(Debug)
-// impl std::fmt::Debug for TextFile {
+// impl std::fmt::Debug for ReadFile {
 //     fn fmt(&self,f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         write!(f,"TextFile{path:{:?},body:{:?}}",&self.path,&self.body)
+//         write!(f,"ReadFile{path:{:?},body:{:?}}",&self.path,&self.body)
 //     }
 // }
-
-
-pub fn read(path: &str) -> Result<TextFile,String> {
-    let f = abspath(path);
-    match f {
-        Ok(p) => read_to_string(p),
-        Err(e) => Err(format!("{:}",e))
-    }
-}
 
 pub fn abspath(relative_path: &str) -> Result<std::path::PathBuf, std::io::Error> {
     let path = PathBuf::from(relative_path);
     return fs::canonicalize(path);
 }
 
-fn read_to_string(p: PathBuf) -> Result<TextFile,String> {
-    if p.is_file() {
-        let path = p.to_str().unwrap();
-        let mut s = String::new();
-        let f = File::open(path);
-        if f.is_err() {
-            return Err(format!("{:}",&f.unwrap_err()))
-        }
-        &f.unwrap().read_to_string(&mut s);
-        return Ok(TextFile::new(path,&s));
+pub fn read_file(path: &str) -> Result<ReadFile,String> {
+    let f = abspath(path);
+    match f {
+        Err(err) => Err(format!("{:}",err)),
+        Ok(pathbuf) => read_path(pathbuf)
+    }
+}
+
+fn read_path(pathbuf: PathBuf) -> Result<ReadFile,String> {
+    if !pathbuf.is_file() {
+        return Err(String::from("No such file"));
     } else {
-        let m = "No such file";
-        return Err(m.to_string());
+        return read_body(pathbuf)
+    }
+}
+
+fn read_body(pathbuf: PathBuf) -> Result<ReadFile,String> {
+    let path = pathbuf.to_str().unwrap();
+    let file = File::open(path);
+    match file {
+        Err(err) => Err(format!("{:}",err)),
+        Ok(mut f) => {
+            let mut s = String::new();
+            &f.read_to_string(&mut s);
+            return Ok(ReadFile::new(path,&s));
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod file;
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     for a in &args {
-        println!("{:?}",file::read(a));
+        println!("{:?}",file::read_file(a));
     }
 }
