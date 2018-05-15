8ffc290924f86177b4a3adfddd84aa15e8679700
Daigo Kawasaki <kawasaki@fujisan.co.jp>
2018-05-15 18:38:02 +0900

    Poundの設定用の構造体を書き始めた

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 mod os;
+mod pound_config;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     for a in &args {
         println!("{:?}", os::file::read(a));
+        println!("{:}",pound_config::Emergency {address: "test".to_string(), port: 100})
     }
 }
---
+++ src/pound_config.rs
@@ -0,0 +1,85 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Emergency {
+    pub address: String,
+    pub port: i32,
+}
+
+impl fmt::Display for Emergency {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Emergency").unwrap();
+        writeln!(f, "  Address {}",  &self.address).unwrap();
+        writeln!(f, "  Port    {}", &self.port).unwrap();
+        writeln!(f, "End").unwrap();
+        return writeln!(f,"");
+    }
+}
+
+#[derive(Debug)]
+pub struct Session {
+    pub kind: SessionKind,
+    pub id: String,
+    pub ttl: i32,
+}
+
+impl fmt::Display for Session {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Session").unwrap();
+        writeln!(f, "  Type {}",  &self.kind.as_str()).unwrap();
+        writeln!(f, "  ID   {:}", &self.id).unwrap();
+        writeln!(f, "  TTL  {:}", &self.ttl).unwrap();
+        writeln!(f, "End").unwrap();
+        return writeln!(f, "");
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[allow(deprecated)]
+pub enum SessionKind {
+    IP,
+    Basic,
+    URL,
+    Parm,
+    Cookie,
+    Header,
+}
+
+impl SessionKind {
+    fn as_str(&self) -> &'static str {
+        match *self {
+            SessionKind::IP => "IP",
+            SessionKind::Basic => "BASIC",
+            SessionKind::URL => "URL",
+            SessionKind::Parm => "PARM",
+            SessionKind::Cookie => "COOKIE",
+            SessionKind::Header => "HEADER",
+        }
+    }
+}
+
+// #[derive(Debug,Eq,PartialEq)]
+// pub struct BackEnd {
+//     address: String,
+//     port: i32,
+//     disabled: bool,
+//     cipher: Option<String>,
+//     priority: Option<i8>,
+//     time_out: Option<i32>,
+//     connection_time_out: Option<i32>,
+// }
+
+// #[derive(Debug)]
+// pub struct Services {
+//     url: String,
+//     ignore_case: Option<bool>,
+//     head_require: Option<String>,
+//     head_deny: Option<String>,
+//     dyn_scale: Option<bool>,
+//     disabled: Option<bool>,
+//     back_end: Option<Vec<BackEnd>>,
+//     redirect: Option<(i32,String)>,
+//     emergency: Option<Emergency>,
+//     session: Option<Session>,
+// }
+
